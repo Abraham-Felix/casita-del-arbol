c0460964f5a505bfe42c40e57b6a01a857d498b2
abraham felix <apesyntax@gmail.com>
2022-11-29 03:12:46 -0700

    making content mobile friendly

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <Nav></Nav>
-    <div class="p-12 body-1 dark-bg light-bg-1 w-full h-full min-h-screen flex flex-col items-center"> 
+    <div class="p-3 md:p-12 body-1 shrink dark-bg light-bg-1 w-full h-full min-h-screen flex flex-col items-center"> 
         <slot />
     </div>
 </template>
--- pages/cuentos-biblicos/[...slug].vue
+++ pages/cuentos-biblicos/[...slug].vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <div class=" mb-12 " >
+    <div class="prose mb-12 " >
       <h1 class="text-lg">  
           <span class="font-bold dark-text light-text-1"> Cuentos Biblicos </span>        
       </h1>
     </div>
-    <div class="flex flex-row justify-center flex-grow">
-      <div class="prose mr-4 p-8 light-bg dark-bg-1 rounded-md min-w-[20ch] max-w-[30ch] flex flex-col">
+    <div class="md:flex flex-row justify-center flex-grow">
+      <div class="prose md:mr-4 mb-3 p-3 md:p-8 light-bg dark-bg-1 rounded-md min-w-[15ch] md:min-w-[20ch] md:max-w-[30ch] flex flex-col">
         <!-- All the lessons for the course listed here -->
         <BlogNav/>
         <!-- <Glosario/> -->
       </div>
-      <div class="prose p-12 light-bg dark-bg-1 rounded-md w-[65ch]">
+      <div class="prose flex flex-row flex-grow p-3 md:p-12 light-bg dark-bg-1 rounded-md shrink md:w-[65ch]">
         <ContentDoc class="articles-content " />
       </div>
     </div>
